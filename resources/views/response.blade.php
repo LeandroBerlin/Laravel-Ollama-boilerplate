7bc9c08840fa5e060bd4eae44013f3895df52280
LeandroBerlin <hello@leandro.berlin>
2023-12-16 23:26:25 +0100

    add: style fixes

--- resources/views/response.blade.php
+++ resources/views/response.blade.php
@@ -1,4 +1,4 @@
-<h2 class="mt-8 mb-4 text-xl font-semibold text-gray-900 dark:text-white">{{$promptText}}</h2>
+<h2 class="capitalize mb-8 text-xl font-semibold text-gray-900 dark:text-white">{{$promptText}}</h2>
 
 <div id="response">
     <x-markdown class="dark:text-white">
